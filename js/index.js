472080e1b2339de5364c2de290bc519cab6991c6
IceBlinks <60108278+IceBlinks@users.noreply.github.com>
2020-03-01 02:52:40 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -38,7 +38,62 @@ function onLoad(){
 }
 
 function onDeviceReady(){
-	refreshDeviceList();
+	// Check if device supports fingerprint
+/**
+* @return {
+*      isAvailable:boolean,
+*      isHardwareDetected:boolean,
+*      hasEnrolledFingerprints:boolean
+*   }
+*/
+FingerprintAuth.isAvailable(function (result) {
+
+    console.log("FingerprintAuth available: " + JSON.stringify(result));
+    
+    // If has fingerprint device and has fingerprints registered
+    if (result.isAvailable == true && result.hasEnrolledFingerprints == true) {
+
+        // Check the docs to know more about the encryptConfig object :)
+        var encryptConfig = {
+            clientId: "dk.kea.BluifruitTest",
+            username: "currentUser",
+            password: "currentUserPassword",
+            maxAttempts: 5,
+            locale: "en_US",
+            dialogTitle: "Hey dude, your finger",
+            dialogMessage: "Put your finger on the device",
+            dialogHint: "No one will steal your identity, promised"
+        }; // See config object for required parameters
+
+        // Set config and success callback
+        FingerprintAuth.encrypt(encryptConfig, function(_fingerResult){
+            console.log("successCallback(): " + JSON.stringify(_fingerResult));
+            if (_fingerResult.withFingerprint) {
+                console.log("Successfully encrypted credentials.");
+                console.log("Encrypted credentials: " + result.token);  
+            } else if (_fingerResult.withBackup) {
+                console.log("Authenticated with backup password");
+            }
+        // Error callback
+        }, function(err){
+                if (err === "Cancelled") {
+                console.log("FingerprintAuth Dialog Cancelled!");
+            } else {
+                console.log("FingerprintAuth Error: " + err);
+            }
+        });
+    }
+
+/**
+* @return {
+*      isAvailable:boolean,
+*      isHardwareDetected:boolean,
+*      hasEnrolledFingerprints:boolean
+*   }
+*/
+}, function (message) {
+    console.log("isAvailableError(): " + message);
+});
 	
 }
 
@@ -116,5 +171,6 @@ function onDisconnect(){
 function onError(reason)  {
 	alert("ERROR: " + reason); // real apps should use notification.alert
 }
+
 
 	
