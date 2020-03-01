1a657b26884e5f8d79f81b481d16388e4a4f733d
IceBlinks <60108278+IceBlinks@users.noreply.github.com>
2020-03-01 11:39:22 +0100

    Update index.js

--- test/www/js/index.js
+++ test/www/js/index.js
@@ -44,3 +44,39 @@ var app = {
 };
 
 app.initialize();
+
+{
+	import { AndroidFingerprintAuth } from '@ionic-native/android-fingerprint-auth';
+
+constructor(private androidFingerprintAuth: AndroidFingerprintAuth) { }
+
+...
+
+
+this.androidFingerprintAuth.isAvailable()
+  .then((result)=> {
+    if(result.isAvailable){
+      // it is available
+
+      this.androidFingerprintAuth.encrypt({ clientId: 'myAppName', username: 'myUsername', password: 'myPassword' })
+        .then(result => {
+           if (result.withFingerprint) {
+               console.log('Successfully encrypted credentials.');
+               console.log('Encrypted credentials: ' + result.token);
+           } else if (result.withBackup) {
+             console.log('Successfully authenticated with backup password!');
+           } else console.log('Didn\'t authenticate!');
+        })
+        .catch(error => {
+           if (error === this.androidFingerprintAuth.ERRORS.FINGERPRINT_CANCELLED) {
+             console.log('Fingerprint authentication cancelled');
+           } else console.error(error)
+        });
+
+    } else {
+      // fingerprint auth isn't available
+    }
+  })
+  .catch(error => console.error(error));
+	
+}
