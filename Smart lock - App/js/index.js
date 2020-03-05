cb769540fda908f03991d7bf80bcbed0a5ff058a
IceBlinks <60108278+IceBlinks@users.noreply.github.com>
2020-03-05 23:13:07 +0100

    Update index.js

--- Smart lock - App/js/index.js
+++ Smart lock - App/js/index.js
@@ -22,7 +22,7 @@ function tryAutoConnect() {
 	//let deviceid = window.localStorage.getItem('deviceid');
 	//if (deviceid !== null) {
 	ble.autoConnect('FC:25:D3:EB:C0:A1', onAutoSucess, onAutoFail);
-	}
+	
 }
 
 function onAutoSucess() {
